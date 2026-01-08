5c60672feed793ea5f0391b0f0f057c3b5aa1e37
LeryCG <valerychacin@gmail.com>
2026-01-08 22:04:09 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 // Interacción de ejemplo
 document.querySelectorAll("button").forEach((btn) => {
-  btn.addEventListener("click", () => {;
-  });
+  btn.addEventListener("click", () => {});
 });
 console.log("Glassmorphism + Parallax activo ✨");
 // Feedback táctil extra al click
@@ -109,4 +108,3 @@ toggle.addEventListener("change", () => {
     document.body.classList.contains("dark") ? "dark" : "light"
   );
 });
-
